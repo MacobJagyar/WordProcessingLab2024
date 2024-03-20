e92d705a6f941117863a67e5b85d5ce54e1ec258
MacobJagyar <jacobmagyar95@gmail.com>
2024-03-20 11:42:55 -0400

    First push

---
+++ WordProcessingLab2024/Program.cs
@@ -0,0 +1,34 @@
+﻿while (true)
+{
+    Console.Write("Hello! Please enter a sentence: ");
+    string input = Console.ReadLine();
+    string[] inputArr = input.Split(" ");
+
+    foreach (string s in inputArr)
+    {
+        Console.WriteLine(s);
+    }
+
+    Console.Write("\nWould you like to try again? (y/n) ");
+    string loopchoice = Console.ReadLine().ToLower();
+
+    if (loopchoice == "y")
+    {
+        Console.WriteLine("\nRestarting...");
+        Thread.Sleep(1500);
+        Console.Clear();
+    }
+    else if (loopchoice == "n")
+    {
+        Console.WriteLine("\nGoodbye!");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("\nThat wasn't y or n! Restarting anyways...");
+        Thread.Sleep(1500);
+        Console.Clear();
+    }
+
+
+}
